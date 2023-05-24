b0b19e89b7f6da661f8bf4c01884ebb02df4f153
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-05-25 01:35:58 +0530

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+
+
+export default function Home() {
+  return (
+  <>
+    <main>
+      <h1>
+        Hello World 
+      </h1>
+    </main>
+  
+  </>
+  )
+}
